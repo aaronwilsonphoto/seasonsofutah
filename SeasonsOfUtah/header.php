a44e66a491f39cc88d00170789a850ab732c865c
Aaron Wilson <aaronwilsonphoto@gmail.com>
2018-03-03 17:32:34 -0700

    Initial Commit with Homepage bilt

---
+++ SeasonsOfUtah/header.php
@@ -0,0 +1,53 @@
+
+      <nav class="navbar navbar-toggleable-md navbar-light" style="background-color: white;">
+
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+          <span class="navbar-toggler-icon"></span>
+
+        </button>
+<div class="container">
+
+        <a class="navbar-brand">Seasons Of Utah</a>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+          <ul class="navbar-nav mr-auto">
+
+            <li <?php echo ($page == 'home') ? 'class="active nav-item"' : '';?>>
+
+              <a class="nav-link" href="index.php">Home</a>
+
+            </li>
+
+            <li <?php echo ($page == 'info') ? 'class="active nav-item"' : '';?>>
+
+              <a class="nav-link" href="#">Information</a>
+
+            </li>
+
+            <li <?php echo ($page == 'submission') ? 'class="active nav-item"' : '';?>>
+
+              <a class="nav-link" href="#">Submission</a>
+
+            </li>
+
+            <li <?php echo ($page == 'terms') ? 'class="active nav-item"' : '';?>>
+
+              <a class="nav-link" href="#">Terms & Conditions</a>
+
+            </li>
+
+            <li <?php echo ($page == 'contact') ? 'class="active nav-item"' : '';?>>
+
+              <a class="nav-link" href="#">Contact</a>
+
+            </li>
+
+          </ul>
+
+        </div>
+
+</div>
+
+      </nav>
---
+++ SeasonsOfUtah/index.php
@@ -0,0 +1,118 @@
+<?php
+$page = 'home';
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+  <head>
+
+    <!-- Required meta tags -->
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+
+    <!-- Custom CSS -->
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+
+    <!-- Google Font -->
+
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400" rel="stylesheet"> 
+    
+    <!-- favicon -->
+    
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+
+  <title>Seasons Of Utah</title>
+
+  </head>
+
+  <body>
+
+  <?php include 'header.php';?>
+
+
+    <section class="bgimage">
+
+      <div class="container-fluid">
+
+        <div class="row">
+
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+            <br>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+
+<div class="container"> 
+
+    <br />
+
+    <h4>Welcome to Seasons Of Utah <br><small>A collection of your stories by Linnette Deason</small></h4>
+
+    <hr />
+
+    <br>
+
+<div class="row">
+  <div class="col-sm-3">
+    <img src="02.jpg" class="rounded float-left" style="max-width: 110%;" alt="...">
+    <img src="01.jpg" class="rounded float-left" style="max-width: 110%;" alt="...">
+    <img src="02.jpg" class="rounded float-left" style="max-width: 110%;" alt="...">
+  </div>
+
+  <div class="col-sm-9">
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">About</h1>
+        <br />
+        <p class="lead">
+        Years back, I saw a father raking leaves with his young son. And I smiled.
+        </p>
+        <p class="lead">
+        Weeks later I passed an older gentleman riding an old-fashioned bicycle. And I smiled.
+        </p>
+        <p class="lead">
+        That’s when I came up with this idea. Humans are exquisite when we have no idea we are being watched. Wouldn’t it be fun to do a book showing the splendid beauty of humanity?
+        </p>
+        <p class="lead">
+        However, EVERY SINGLE TIME I bore witness to the magnificent humans surrounding me, the book came forward. I long to create this book. Therefore, I turn to you. Be my eyes, my legs and the vision behind every aspect of this book.
+        </p>
+        <p class="lead">
+        You know your family and friends and neighbors and neighborhood. We live in an age where nearly every single person photographs daily life. Let’s make a book of our pictures and bear witness to our stunning surroundings!
+        </p>
+        <p class="lead">
+        I live in Utah, so that’s where I’ll begin. From the deserts of Southern Utah to the moutainous views of the Wasatch range, the nature surrounding Utah is marvelous beyond measure; coupled with human form, my hope is to compile a collection that is equally intriguing and beauiful.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="filler-container"></div>
+</div>
+
+    <div class="filler-container"></div>
+    </div> <!-- End Container -->
+
+    <?php include 'footer.php';?>
+
+  </body>
+
+</html>
